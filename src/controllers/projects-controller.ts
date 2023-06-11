e0d1b4ff134d80ace87f472faa8d3257d4c840ce
Alexandre de Freitas Souza <alexandrefsoficial@gmail.com>
2023-06-11 01:36:08 -0300

    refactor: useless param removed

--- src/controllers/projects-controller.ts
+++ src/controllers/projects-controller.ts
@@ -4,7 +4,6 @@ import { AuthenticatedRequest } from '@/middlewares';
 import projectService from '@/services/projects-service';
 
 export async function getProjects(req: AuthenticatedRequest, res: Response) {
-  const { userId } = req;
   try {
     const result = await projectService.getProjects();
     return res.status(httpStatus.OK).send(result);
