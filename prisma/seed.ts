09d4ed464a9b13038a25e053b64b208d3f56c5e6
Alexandre de Freitas Souza <alexandrefsoficial@gmail.com>
2023-06-26 13:45:40 -0300

    Refactor: seed function divided by table

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,39 +2,57 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function seed() {
-  const universities = [
-    "UNB - Universidade de Brasília",
-    "UFMG - Universidade Federal de Minas Gerais",
-    "UFBA - Universidade Federal da Bahia",
-    "UniCeub - Centro Universitário de Brasília",
-  ];
-
+async function seedExpertise() {
   const expertise = [
     "Engenharia Civil",
     "Medicina",
     "Direito",
     "Administração",
   ];
+  const expertisePromises = expertise.map(async (name) => {
+    await prisma.expertise.deleteMany({
+      where: {
+        name: name,
+      },
+    });
 
-  const universityPromises = universities.map((name) =>
-    prisma.university.create({
+    await prisma.expertise.create({
       data: {
         name,
       },
-    })
-  );
+    });
+  });
 
-  const expertisePromises = expertise.map((name) =>
-    prisma.expertise.create({
+  await Promise.all(expertisePromises);
+}
+
+async function seedUniversity() {
+  const universities = [
+    "UNB - Universidade de Brasília",
+    "UFMG - Universidade Federal de Minas Gerais",
+    "UFBA - Universidade Federal da Bahia",
+    "UniCeub - Centro Universitário de Brasília",
+  ];
+  const universityPromises = universities.map(async (name) => {
+    await prisma.university.deleteMany({
+      where: {
+        name: name,
+      },
+    });
+
+    await prisma.university.create({
       data: {
         name,
       },
-    })
-  );
+    });
+  });
 
-  await Promise.all([...universityPromises, ...expertisePromises]);
+  await Promise.all(universityPromises);
+}
 
+async function seed() {
+  await seedExpertise();
+  await seedUniversity();
   console.log("Dados de seed inseridos com sucesso!");
 }
 
